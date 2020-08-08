f36853a4d445a0f978a68c213ab4ebd4399d1002
Darius28 <68659072+Darius28@users.noreply.github.com>
2020-08-08 14:21:11 +0530

    Update SpeakGoodbye.js

--- SpeakGoodbye.js
+++ SpeakGoodbye.js
@@ -1,7 +1,8 @@
-(function()
+(function(window)
  {
   var speakWord = "Good Bye";
   var byeSpeaker = function speak(name) {
   console.log(speakWord + " " + name);
 }
- })();
+  window.byeSpeaker = byeSpeaker;
+ })(window);
