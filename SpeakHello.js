f34ae9bf6956b4e294c2877fa2a231aa3749c68e
Darius28 <68659072+Darius28@users.noreply.github.com>
2020-08-08 14:20:27 +0530

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,7 +1,8 @@
-(function()
+(function(window)
  {
   var speakWord = "Hello";
   var helloSpeaker = function speak(name) {
   console.log(speakWord + " " + name);
 }
- })();
+  helloSpeaker.window = helloSpeaker;
+ })(window);
